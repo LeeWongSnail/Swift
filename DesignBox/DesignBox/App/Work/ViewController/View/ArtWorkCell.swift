92d8a337011d743b6b1483db68453dd3afffbcc7
cainvLuo <624757635@qq.com>
2017-08-16 19:45:36 +0800

    add image kingfisher

---
+++ DesignBox/DesignBox/App/Work/ViewController/View/ArtWorkCell.swift
@@ -0,0 +1,36 @@
+//
+//  ArtWorkCell.swift
+//  DesignBox
+//
+//  Created by LeeWong on 2017/8/16.
+//  Copyright © 2017年 LeeWong. All rights reserved.
+//
+
+import UIKit
+
+class ArtWorkCell: UITableViewCell {
+
+    
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    //MARK: - Lazy Load
+    
+    
+    
+
+}
