718dd4bc778fcd9543faf620ab23f3a45ae2b42f
cainvLuo <624757635@qq.com>
2017-09-06 17:46:20 +0800

    add requirement cell xib

---
+++ DesignBox/DesignBox/App/Requirement/ViewController/ArtRequirementListController.swift
@@ -0,0 +1,26 @@
+//
+//  ArtRequirementListController.swift
+//  DesignBox
+//
+//  Created by LeeWong on 2017/9/6.
+//  Copyright © 2017年 LeeWong. All rights reserved.
+//
+
+import UIKit
+
+class ArtRequirementListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
---
+++ DesignBox/DesignBox/App/Requirement/ViewController/View/ArtRequirementCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArtRequirementCell.swift
+//  DesignBox
+//
+//  Created by LeeWong on 2017/9/6.
+//  Copyright © 2017年 LeeWong. All rights reserved.
+//
+
+import UIKit
+
+class ArtRequirementCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
